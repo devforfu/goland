df66a46c940f59de9731154d2d1e2f62cbcd5281
devforfu <developer.z@outlook.com>
2018-07-03 22:05:57 +0500

    Server script

--- playground/server1.go
+++ playground/server1.go
@@ -4,13 +4,43 @@ import (
 	"net/http"
 	"log"
 	"fmt"
+	"sync"
 )
+
+var mu sync.Mutex
+var count int
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	http.HandleFunc("/echo", echo)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count += 1
+	mu.Unlock()
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n",r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
 }
