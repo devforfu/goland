0a780f02816a8941b1d5c6046142071693d2f134
devforfu <developer.z@outlook.com>
2018-10-08 22:35:20 +0500

    Reverse inplace

--- playground/scratch.go
+++ playground/scratch.go
@@ -2,30 +2,14 @@ package main
 
 import "fmt"
 
-type Person struct {
-	firstName string
-	lastName string
-	age int
-}
-
-type Printable interface {
-	Describe() string
-}
-
-func (p Person) Describe() string {
-	return fmt.Sprintf("%s %s", p.firstName, p.lastName)
-}
-
-func fib(n int) int {
-	x, y := 0, 1
-	for i := 0; i < n; i++ {
-		x, y = y, x + y
-	}
-	return x
-}
-
 func main() {
-	person := Person{"John", "Doe", 21}
-	fmt.Printf("%v\n", person.Describe())
-	println(fib(10))
+    xs := [...]int{1, 2, 3, 4, 5}
+    reverse(xs[:])
+    fmt.Printf("%v", xs)
+}
+
+func reverse(values []int) {
+    for i, j := 0, len(values) - 1; i < j; i, j = i + 1, j - 1 {
+        values[i], values[j] = values[j], values[i]
+    }
 }
