238e06059ee6023d5954116593e934d822c627bf
devforfu <developer.z@outlook.com>
2018-10-20 23:18:05 +0500

    Book examples

--- crawler/findlinks.go
+++ crawler/findlinks.go
@@ -19,25 +19,15 @@ func main() {
 
 // visit appends to links each link found in n and returns the result.
 func visit(links []string, n *html.Node) []string {
-    if n.Type == html.ElementNode && n.Data == "a" {
-        for _, a := range n.Attr {
-            if a.Key == "href" {
-                links = append(links, a.Val)
-            }
-        }
-    }
-    for c := n.FirstChild; c != nil; c = c.NextSibling {
-        links = visit(links, c)
-    }
-    return links
-}
-
-func outline(stack []string, n *html.Node) {
-    if n.Type == html.ElementNode {
-        stack = append(stack, n.Data)
-        fmt.Println(stack)
-    }
-    for c := n.FirstChild; c != nil; c = c.NextSibling {
-        outline(stack, c)
-    }
+   if n.Type == html.ElementNode && n.Data == "a" {
+       for _, a := range n.Attr {
+           if a.Key == "href" {
+               links = append(links, a.Val)
+           }
+       }
+   }
+   for c := n.FirstChild; c != nil; c = c.NextSibling {
+       links = visit(links, c)
+   }
+   return links
 }
